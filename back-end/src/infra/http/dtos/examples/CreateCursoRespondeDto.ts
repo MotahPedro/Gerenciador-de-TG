bd49abf263bcbeaa74c4e5eac381b626a190d59c
MotahPedro <motahpedro@gmail.com>
2025-03-19 21:13:20 -0300

    feat: Created Curso Controller

---
+++ back-end/src/infra/http/dtos/examples/CreateCursoRespondeDto.ts
@@ -0,0 +1,6 @@
+export const createCursoResponseExample = {
+    id: 1,
+    curso: "Analise e Desenvolvimento de Software",
+    professorOrientadorCpf: "123456789012",
+    cpfs: '["123456789012", "321321421512"]'
+};
